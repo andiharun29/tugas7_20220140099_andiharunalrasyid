1127502ff4f3e97275cc20ff9382ecf177ada6d5
unknown <andiharunlrasyid14@gmail.com>
2024-04-07 13:19:18 +0800

    tugas pdw video dan audio

---
+++ script.js
@@ -0,0 +1,12 @@
+video.addEventListener("click", function video(){
+    alert("ini cuplikan video chant liverpool di anfield");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini audio chant liverpool di anfield");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini adalah liverpool FC");
+});
+
